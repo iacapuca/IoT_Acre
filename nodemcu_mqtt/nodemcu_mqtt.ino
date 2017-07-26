c448a22d7f83572b8515a82278e614d16463edda
Pedro Luiz Paes Barreto dos Santos <iacapuca@gmail.com>
2017-07-26 14:11:50 -0300

    Adição de Sensores e codigo agnostico

--- nodemcu_mqtt/nodemcu_mqtt.ino
+++ nodemcu_mqtt/nodemcu_mqtt.ino
@@ -1,26 +1,27 @@
 /**
-  Acre IoT Workshop - NodeMCU
-  Autor: Pedro Luiz @iacapuca
+   Acre IoT Workshop - NodeMCU
+   Autor: Pedro Luiz @iacapuca
 
-*/
+ */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 //#include <ArduinoJson.h>
 #include <DHT.h>
+#include "MQ135.h"
 
 
 //-------- Wi-Fi -----------
-const char* ssid = "Euler";
-const char* password = "e^(i*pi)+1=0";
+const char* ssid = "";
+const char* password = "";
 
 //-------- MQTT ------------
 #define ORG "2kcheh"
 #define DEVICE_TYPE "nodemcu"
-#define DEVICE_ID "5CCF7F248AA7"
-#define TOKEN "v6lDrq0kqLWUzLFmHs"
+#define DEVICE_ID "" // MAC ADDRESS
+#define TOKEN "" // TOKEN DO BLUEMIX
 
-char server[] = ORG".messaging.internetofthings.ibmcloud.com";
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
 char topic[] = "iot-2/evt/status/fmt/json";
 char authMethod[] = "use-token-auth";
 char token[] = TOKEN;
@@ -31,6 +32,10 @@ PubSubClient client(server, 1883, NULL, wifiClient);
 
 //-------- DEBUG ONLY -------
 
+//-------- GEOLOCATION ------
+float lat;
+float lng;
+
 //-------- DHT11 ------------
 #define DHTPIN 5 // pino que estamos conectado
 #define DHTTYPE DHT11 // DHT 11
@@ -38,73 +43,139 @@ DHT dht(DHTPIN, DHTTYPE);
 float umi;
 float temp;
 
+//-------- PIR --------------
+#define PIRPIN 16
+int pirValue;
+
+//-------- CHUVA ------------
+#define RAINPIN 4
+int rainValue;
+
+//-------- MQ135 ------------
+#define ANALOGPIN A0
+MQ135 gasSensor = MQ135(ANALOGPIN);
+
+//-------- MQ2 --------------
+#define MQ2PIN 2
+int mq2Value;
+
 // functions
 void getData();
+void tempData();
+void pirData();
+void rainData();
+void mq135Data();
+void mq2Data();
 
-//------- void setuo() ------
+//------- void setup() ------
 void setup(){
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Connecting to "); Serial.print(ssid);
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-   Serial.print(".");
-   }
-   Serial.println("");
+        Serial.begin(115200);
+        Serial.println();
+        Serial.print("Connecting to "); Serial.print(ssid);
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+                delay(500);
+                Serial.print(".");
+        }
+        Serial.println("");
 
-   Serial.print("WiFi connected, IP address: "); Serial.println(WiFi.localIP());
-   dht.begin();
+        Serial.print("WiFi connected, IP address: "); Serial.println(WiFi.localIP());
+        dht.begin();
+        pinMode(PIRPIN, INPUT);
+        pinMode(RAINPIN, INPUT);
 
 }
 
 void loop(){
-  if (!client.connected()) {
- Serial.print("Reconnecting client to ");
- Serial.println(server);
- while (!client.connect(clientId, authMethod, token)) {
- Serial.print(".");
- delay(500);
- }
- Serial.println();
- }
+        if (!client.connected()) {
+                Serial.print("Reconnecting client to ");
+                Serial.println(server);
+                while (!client.connect(clientId, authMethod, token)) {
+                        Serial.print(".");
+                        delay(500);
+                }
+                Serial.println();
+        }
 
-getData();
+        getData();
 
-   //Logs na porta serial
-     Serial.print("Temperatura: ");
-     Serial.print(temp);
-     Serial.print(" Umidade: ");
-     Serial.println(umi);
+        //Logs na porta serial
+        Serial.print("Temperatura: ");
+        Serial.print(temp);
+        Serial.print(" Umidade: ");
+        Serial.println(umi);
+        Serial.println("PIR: ");
+        Serial.print(pirValue);
+        Serial.println("Chuva: ");
+        Serial.print(rainValue);
+        Serial.println("MQ135: ");
+        Serial.print(rzero);
+        Serial.println("MQ2: ");
+        Serial.print(mq2Value);
 
 
- String payload = "{\"acre1\":{\"Name\":\"5CCF7F248AA7\"";
- payload += ",\"temp\":";
- payload += temp;
- payload += ",\"umi\":";
- payload += umi;
- payload += "}}";
 
- Serial.print("Sending payload: ");
- Serial.println(payload);
+        String payload = "{\"acre1\":{\"Name\":\"5CCF7F248AA7\"";
+        payload += ",\"temp\":";
+        payload += temp;
+        payload += ",\"umi\":";
+        payload += umi;
+        payload += ",\"pir\":";
+        payload += pirValue;
+        payload += ",\"rain\":";
+        payload += rainValue;
+        payload += ",\"mq2\":";
+        payload += mq2Value;
+        payload += "}}";
 
- if (client.publish(topic, (char*) payload.c_str())) {
- Serial.println("Publish ok");
- } else {
- Serial.println("Publish failed");
- }
+        Serial.print("Sending payload: ");
+        Serial.println(payload);
 
- delay(300000);
+        if (client.publish(topic, (char*) payload.c_str())) {
+                Serial.println("Publish ok");
+        } else {
+                Serial.println("Publish failed");
+        }
+
+        delay(300000);
 }
 
 void getData(){
-  //Leitura de umidade
-  float umi = dht.readHumidity();
-  //Leitura de temperatura
-  float temp = dht.readTemperature();
-  //Se não for um numero retorna erro de leitura
-  if (isnan(umi) || isnan(temp)) {
-    Serial.println("Erro ao ler o sensor!");
-    return;
-  }
+        tempData();
+        pirData();
+        rainData();
+        mq135Data();
+        mq2Data();
+}
+
+void tempData(){
+        //Leitura de umidade
+        float umi = dht.readHumidity();
+        //Leitura de temperatura
+        float temp = dht.readTemperature();
+        //Se não for um numero retorna erro de leitura
+        if (isnan(umi) || isnan(temp)) {
+                Serial.println("Erro ao ler o sensor!");
+                return;
+        }
+}
+
+void pirData(){
+        //Leitura do PIR
+        pirValue = digitalRead(PIRPIN);
+}
+
+void rainData(){
+    //Leitura do Sensor de Chuva
+    rainValue = digitalRead(RAINPIN);
+}
+
+void mq135Data(){
+  //BURN do MQ135
+  float rzero = gasSensor.getRZero();
+}
+
+void mq2Data(){
+  //Leitura digital do Sensor mq2Data
+  mq2Value = digitalRead(MQ2PIN);
 }
